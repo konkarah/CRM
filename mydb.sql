4818c93505bf194f474b1adeb175f94e6d9d289d
konkarah <KONKARAHDEH@GMAIL.COM>
2023-10-13 16:02:40 +0300

    added db

--- mydb.sql
+++ mydb.sql
@@ -2,16 +2,19 @@ CREATE TABLE CALLCENTRECASES(
     id INT AUTOINCREMENT,
     name varchar(60),
     idno BIGINT,
+    progno varchar(30),
     prog varchar(10),
     sex varchar(8),
     phone varchar(30),
     compsrc varchar(30),
     casecat varchar(30),
     casetype varchar(30),
-    desc varchar(255),
+    descr varchar(255),
     mood INT,
     status varchar(20),
-    resolution varchar(255)
+    resolution varchar(255),
+    logger varchar(60),
+    mydate varchar(30),
 
     PRIMARY KEY (id)
 )
--- server.js
+++ server.js
@@ -14,6 +14,22 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/mycallcentreUI', (req,res)=> {
+    const id = req.body.id
+    Db.query(
+        "SELECT * FROM ctovcmysql WHERE PrimaryRecipientNationalIDNo = ? OR ProgrammeNo = ?",
+        [id,id],
+        (err, result) => {
+            if(err){
+                console.log(err)
+                return err
+            }else{
+                res.send(result)
+            }
+          }
+    )
+})
+
 app.post('/mycallcentre', (req,res)=> {
     const name = req.body.name
     const id= req.body.idno
@@ -21,13 +37,36 @@ app.post('/mycallcentre', (req,res)=> {
     const prog= req.body.prog
     const sex= req.body.sex
     const phone= req.body.phone
-    const compsrc= req.body.cpmpsrc
-    const compcat= req.body.compcat
+    const compsrc= req.body.compsrc
+    const casecat= req.body.compcat
     const casetype= req.body.casetype
     const desc= req.body.desc
     const mood= req.body.mood
     const status= req.body.status
     const resolution= req.body.resolution
+    const logger = "test"
+    const date_now = new Date()
+    const date = ("0" + date_now.getDate()).slice(-2);
+
+    // get current month
+    let month = ("0" + (date_now.getMonth() + 1)).slice(-2);
+
+    // get current year
+    let year = date_now.getFullYear();
+
+    const mydate = date +"-"+month+"-"+year
+    Db.query(
+        "INSERT INTO CALLCENTRECASES(name, idno, progno, prog, sex, phone, compsrc, casecat, casetype, descr, mood, status, resolution, logger, mydate) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+        [name, id, progno, prog, sex, phone, compsrc, casecat, casetype, desc, mood, status, resolution, logger, mydate],
+        (err, result) => {
+            if(err){
+                console.log(err)
+                return err
+            }else{
+                res.send(result)
+            }
+          }
+    )
 })
 
 app.listen(port, () => {
